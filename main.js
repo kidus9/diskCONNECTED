ef3561f96ea3be7dc9faf7e0423fa6c398b4504a
kidus9 <77864368+kidus9@users.noreply.github.com>
2021-09-23 19:37:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+song = "";
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function preload()
+{
+    song = loadSound("music.mp3");
+}
+
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.ceter();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    Image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    song.play();
+}
